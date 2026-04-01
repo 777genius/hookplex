df2fccd68d1d2ca57ef8d6373f0881ff519d710e
iliya <iliyazelenkog@gmail.com>
2026-04-01 22:47:47 +0300

    Use official docs UX packages

---
+++ website/.vitepress/theme/components/Layout.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { useData } from "vitepress";
+import DefaultTheme from "vitepress/theme";
+import { useMermaidZoom } from "vitepress-mermaid-zoom";
+import { useCodeblockCollapse } from "vitepress-codeblock-collapse";
+import LocalePreferenceSync from "./LocalePreferenceSync.vue";
+import "vitepress-mermaid-zoom/style.css";
+import "vitepress-codeblock-collapse/style.css";
+
+const { page } = useData();
+const pagePath = computed(() => page.value.relativePath);
+
+useMermaidZoom(pagePath, {
+  selector: ".mermaid-diagram__surface"
+});
+
+useCodeblockCollapse(pagePath);
+</script>
+
+<template>
+  <DefaultTheme.Layout>
+    <template #layout-top>
+      <LocalePreferenceSync />
+    </template>
+  </DefaultTheme.Layout>
+</template>
--- website/.vitepress/theme/index.ts
+++ website/.vitepress/theme/index.ts
@@ -1,8 +1,7 @@
 import DefaultTheme from "vitepress/theme";
 import type { Theme } from "vitepress";
-import { h } from "vue";
+import Layout from "./components/Layout.vue";
 import LanguageGateway from "./components/LanguageGateway.vue";
-import LocalePreferenceSync from "./components/LocalePreferenceSync.vue";
 import DocMetaCard from "./components/DocMetaCard.vue";
 import MermaidDiagram from "./components/MermaidDiagram.vue";
 import NotFoundLinks from "./components/NotFoundLinks.vue";
@@ -10,13 +9,9 @@ import "./styles/custom.css";
 
 const theme: Theme = {
   ...DefaultTheme,
-  Layout: () =>
-    h(DefaultTheme.Layout, null, {
-      "layout-top": () => h(LocalePreferenceSync)
-    }),
+  Layout,
   enhanceApp({ app }) {
     app.component("LanguageGateway", LanguageGateway);
-    app.component("LocalePreferenceSync", LocalePreferenceSync);
     app.component("DocMetaCard", DocMetaCard);
     app.component("MermaidDiagram", MermaidDiagram);
     app.component("NotFoundLinks", NotFoundLinks);
